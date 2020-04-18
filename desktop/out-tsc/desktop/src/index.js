0da78f9cacdc1c505b0a2becca3fb05d1e053a91
Alison Aguiar <alysson.k888@gmail.com>
2020-04-18 19:52:05 -0300

    add splash loading

--- desktop/out-tsc/desktop/src/index.js
+++ desktop/out-tsc/desktop/src/index.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const electron_1 = require("electron");
 const path_1 = require("path");
 let win;
+let splash;
 function showWindow() {
     function onClose() {
         win = undefined;
@@ -10,18 +11,39 @@ function showWindow() {
     }
     function createWindow() {
         win = new electron_1.BrowserWindow({
+            title: 'Titulo do App',
             width: 320,
             height: 300,
+            show: false,
             webPreferences: {
                 preload: path_1.join(__dirname, 'bridge.js'),
                 nodeIntegration: false,
                 nodeIntegrationInWorker: false,
             }
         });
+        splash = new electron_1.BrowserWindow({
+            frame: false,
+            width: 200,
+            height: 200,
+            center: true,
+            transparent: true,
+            resizable: false,
+            alwaysOnTop: true,
+            hasShadow: false,
+            titleBarStyle: 'default',
+            thickFrame: true
+        });
     }
     createWindow();
+    splash.loadFile(path_1.join(__dirname, 'splash.html'));
+    setTimeout(() => {
+        splash.destroy();
+        win.loadURL('http://localhost:4200/');
+        // win.loadFile(join(__dirname, 'splash.html'))
+        win.show();
+    }, 4000);
     win.on('close', onClose);
-    win.loadURL('http://localhost:4200/');
+    // win.loadURL('http://localhost:4200/');
 }
 electron_1.app.allowRendererProcessReuse = true;
 electron_1.app.on('ready', showWindow);
--- desktop/src/index.ts
+++ desktop/src/index.ts
@@ -3,28 +3,55 @@ import { join } from 'path';
 
 
 let win: BrowserWindow | undefined;
+let splash: BrowserWindow | undefined;
 
 function showWindow() {
     function onClose() {
         win = undefined;
         app.exit()
     }
-    
+
     function createWindow() {
         win = new BrowserWindow({
+            title: 'Titulo do App',
             width: 320,
             height: 300,
+            show: false,
             webPreferences: {
                 preload: join(__dirname, 'bridge.js'),
                 nodeIntegration: false,
                 nodeIntegrationInWorker: false,
             }
         });
+        splash = new BrowserWindow({
+            frame: false,
+            width: 200,
+            height: 200,
+            center: true,
+            transparent: true,
+            resizable: false,
+            alwaysOnTop: true,
+            hasShadow: false,
+            titleBarStyle: 'default',
+            thickFrame: true
+        })
     }
 
+
+
     createWindow();
+    splash.loadFile(join(__dirname, 'splash.html'))
+
+    setTimeout(() => {
+        splash.destroy()
+        win.loadURL('http://localhost:4200/');
+
+        // win.loadFile(join(__dirname, 'splash.html'))
+        win.show()
+    }, 4000);
     win.on('close', onClose);
-    win.loadURL('http://localhost:4200/');
+    // win.loadURL('http://localhost:4200/');
+
 
 }
 
